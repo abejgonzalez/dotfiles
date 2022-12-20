9dfde4c4c74dcb4268b9cb2edba00cd2d58969d7
abejgonzalez <abe.j.gonza@gmail.com>
2022-12-19 22:33:22 -0800

    Fix setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -ex
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Setup the initial files based on $SHELL
@@ -9,16 +9,17 @@ SHELL_TYPE=$(basename ${SHELL})
 SHELLRC_FILE=~/.${SHELL_TYPE}rc
 
 echo "export XDG_CONFIG_HOME=$SCRIPT_DIR" >> $SHELLRC_FILE
-echo "source \$CONFIG_DIR/.${SHELL_TYPE}_common" >> $SHELLRC_FILE
-echo "source \$CONFIG_DIR/.${SHELL_TYPE}_prompt" >> $SHELLRC_FILE
+echo "source \$XDG_CONFIG_HOME/.${SHELL_TYPE}_common" >> $SHELLRC_FILE
+echo "source \$XDG_CONFIG_HOME/.${SHELL_TYPE}_prompt" >> $SHELLRC_FILE
 
 # Install nightly version of neovim
 mkdir installs
 pushd installs
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-ln -s nvim.appimage nvim
+ln -sf nvim.appimage nvim
 popd
+echo "export PATH=\$XDG_CONFIG_HOME/installs:\$PATH" >> $SHELLRC_FILE
 
 # Install Vundle
 
@@ -27,11 +28,11 @@ git clone https://github.com/VundleVim/Vundle.vim.git $SCRIPT_DIR/.vim/bundle/Vu
 # Setup firrtl syntax for vim
 
 git submodule update --init --recursive firrtl-syntax
-pushd firrtl-syntax
+pushd firrtl-syntax
 mkdir -p $SCRIPT_DIR/.vim/syntax
 mkdir -p $SCRIPT_DIR/.vim/ftdetect
-ln syntax/firrtl.vim $SCRIPT_DIR/.vim/syntax/firrtl.vim
-ln ftdetect/firrtl.vim $SCRIPT_DIR/.vim/ftdetect/firrtl.vim
+ln -sf syntax/firrtl.vim $SCRIPT_DIR/.vim/syntax/firrtl.vim
+ln -sf ftdetect/firrtl.vim $SCRIPT_DIR/.vim/ftdetect/firrtl.vim
 popd
 
 # add ssh config to .ssh/config
